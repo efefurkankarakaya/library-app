2595d80fa2e98126162376ddfca93ec03a3d5f39
efefurkankarakaya <efkdev@outlook.com>
2023-07-31 17:24:31 +0300

    fix: type errors

--- src/helpers/styleHelpers.ts
+++ src/helpers/styleHelpers.ts
@@ -2,18 +2,18 @@ import { StyleProp, StyleSheet } from "react-native";
 
 // Type Definitions
 type TStyleSheet = StyleProp<object>;
-type TInitial = TStyleSheet | TStyleSheet[];
+// type TInitial = TStyleSheet | TStyleSheet[];
 type TStyle = TStyleSheet | TStyleSheet[];
 
 // Function Definitions
-type TCombineStyles = (initial: TInitial, styleSheet: TStyleSheet) => TStyle;
+type TCombineStyles = (initial: TStyle, upcoming: TStyle) => TStyle;
 
 /**
- * @param {TStyleSheet | TStyleSheet[]} initial - StyleSheet object or an array of StyleSheet objects.
- * @param {TStyleSheet} upcoming - The StyleSheet object to be combined with initial StyleSheets.
+ * @param {TStyle} initial - StyleSheet object or an array of StyleSheet objects.
+ * @param {TStyle} upcoming - The StyleSheet object to be combined with initial StyleSheets.
  * @returns {TStyle} - Returns a merged version of all styles.
  */
-export const combineStyles: TCombineStyles = (initial: TInitial, upcoming: TStyle): TStyle => {
+export const combineStyles: TCombineStyles = (initial: TStyle, upcoming: TStyle): TStyle => {
   /**
    * Upcoming style can be falsy to not to break custom components
    * because these components have optional style props.
